fd820b8bc65cd54c09d3c9c8993694abea82075b
Paroklo <patroklo@gmail.com>
2015-08-15 17:08:37 +0200

    Changes

--- Module.php
+++ Module.php
@@ -8,7 +8,7 @@ class Module extends \atuin\skeleton\Module
 
     protected static $_id = 'apps';
 
-    protected static $_version = '0';
+    protected static $_version = '0.0.1';
 
     public $is_core_module = 1;
 }
